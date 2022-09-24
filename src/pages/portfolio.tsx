d108dd87ec333b67685c6c2537b52d12bb308b16
Reza Hasani <reza.hasani7899@gmail.com>
2022-09-24 21:30:14 +0330

    Added tab section to Portfolio page

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -4,6 +4,32 @@ import AnimationContainer from "../layouts/AnimationContainer";
 import Title from "../components/Title";
 import PortfolioItem from "../components/PortfolioItem";
 import styled from "styled-components";
+import { useState } from "react";
+
+const TabContainer = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 25px !important;
+`;
+
+const TabItem = styled.li`
+  color: #eee;
+  cursor: pointer;
+  list-style: none;
+  outline: none;
+  text-decoration: none;
+  transition: all 0.4s;
+  font-size: 16px;
+  margin: 0 15px 15px;
+  font-family: Poppins, sans-serif;
+  text-transform: uppercase;
+
+  &:hover {
+    color: var(--main-primary-color);
+  }
+`;
 
 const ItemsContainer = styled.div`
   grid-gap: 30px;
@@ -14,12 +40,19 @@ const ItemsContainer = styled.div`
 `;
 
 const Portfolio = () => {
+  const [selectedTab, setSelectedTab] = useState("All");
+
   return (
     <AnimationContainer margin="auto">
       <Title
         frontTitle={{ whitePart: "My", greenPart: "Portfolio" }}
         backTitle="Works"
       />
+      <TabContainer>
+        <TabItem onClick={() => setSelectedTab("All")}>All</TabItem>
+        <TabItem onClick={() => setSelectedTab("Websites")}>Websites</TabItem>
+        <TabItem onClick={() => setSelectedTab("Logos")}>Logos</TabItem>
+      </TabContainer>
       <ItemsContainer>
         <PortfolioItem
           title="Test"
