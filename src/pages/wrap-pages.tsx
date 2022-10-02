19dc9e9aba0fab516e5abff1c19f5b993d94d48e
Reza Hasani <reza.hasani7899@gmail.com>
2022-10-02 13:57:10 +0330

    Added isMobile condition for the cursor

--- src/pages/wrap-pages.tsx
+++ src/pages/wrap-pages.tsx
@@ -4,24 +4,27 @@ import MenuButton from "../components/MenuButton";
 import { useLocation } from "@reach/router";
 import AnimatedCursor from "react-animated-cursor";
 import { Toaster } from "react-hot-toast";
+import { isMobile } from "../../utils/constants";
 
 export function wrapPages({ element }: { element: React.Component }) {
   const location = useLocation();
 
   return (
     <>
-      <AnimatedCursor
-        innerSize={10}
-        outerSize={8}
-        color="255, 160, 1"
-        outerAlpha={0.3}
-        innerScale={0.7}
-        outerScale={6}
-        clickables={["*"]}
-        outerStyle={{
-          mixBlendMode: "color-dodge",
-        }}
-      />
+      {!isMobile() && (
+        <AnimatedCursor
+          innerSize={10}
+          outerSize={8}
+          color="255, 160, 1"
+          outerAlpha={0.3}
+          innerScale={0.7}
+          outerScale={6}
+          clickables={["*"]}
+          outerStyle={{
+            mixBlendMode: "color-dodge",
+          }}
+        />
+      )}
       <GlobalStyles />
       <Toaster />
       <MenuButton route={location.pathname} />
